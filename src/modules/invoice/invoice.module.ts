9f62c909a44eadeb58ce31b940b0a87f41871f08
aminvb12 <amin.kzm12@gmail.com>
2026-06-13 12:36:55 +0200

    feat(invoice): include Company entity in Invoice module imports

--- src/modules/invoice/invoice.module.ts
+++ src/modules/invoice/invoice.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Invoice } from './entities/invoice.entity';
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
+import { Company } from '../company/entities/company.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invoice])],
+  imports: [TypeOrmModule.forFeature([Invoice, Company])],
   controllers: [InvoiceController],
   providers: [InvoiceService],
   exports: [InvoiceService],
